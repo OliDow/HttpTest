402dd8ad6e039e98b02b4460621d24786afae910
Oliver <oli@shatteredsun.com>
2016-11-08 19:23:34 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return Task.Run(async () =>
+            {
+                HttpConnTest connTest = new HttpConnTest(); 
+                var resp = await connTest.Get((args.Length > 0) ? args[0] : "http://shatteredsun.com");
+                Console.WriteLine(resp);
+                return 0;
+            }).Result;
+        }
+    }
+
+    public class HttpConnTest{
+        public async Task<string> Get(string url){
+            try{
+                HttpClient client = new HttpClient();
+                var responce = await client.GetStringAsync(url);
+                return responce;
+            } catch(Exception e){
+                return e.Message;
+            }
+        }
+    }
+}
